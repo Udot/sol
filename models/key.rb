1af76410803fa99a58061880e6b57bced6f0ba94
mcansky <riboulet@gmail.com>
2011-08-27 17:45:30 +0200

    ssh keys management basics

--- models/key.rb
+++ models/key.rb
@@ -5,8 +5,72 @@ class SshKey
   has n, :eggs, :through => Resource
 
   property :id, Serial
-  property :ssh_key, Text
+  property :name, String, :required => true
+  property :ssh_key, Text, :required => true
   property :user_id, Integer
   property :created_at, DateTime
   property :updated_at, DateTime
+
+  # extract the login from the pasted key and insert it in the db
+  def extract_login
+    key_pieces = self.ssh_key.split(" ")
+    return key_pieces[2] if key_pieces.size == 3
+  end
+  alias_method :login, :extract_login
+
+  # return the ssh key (without the username@host)
+  def short
+    key_pieces = self.ssh_key.split(" ")
+    return key_pieces[0] + " " + key_pieces[1]
+  end
+
+  # split up the key in several 20 chars lines
+  def ssh_key_nice
+    nice_key = ""
+    ssh_key.split("\n").each do |key_slice|
+      if key_slice.size < 20
+        nice_key += key_slice + "\n"
+      else
+        i = 0
+        while i < key_slice.size
+          nice_key += key_slice.slice(i..i+20) + "\n"
+          i += 21
+        end
+      end
+    end  
+    return nice_key
+  end
+
+  # class method
+  # validity check
+  def self.valid?(a_key)
+    if a_key
+      key_pieces = a_key.split(" ")
+      if key_pieces.size < 3
+        return false # we want a full three parts ssh key
+      end
+
+      # Test first element
+      if !['ssh-dss', 'ssh-rsa'].include? key_pieces[0]
+        return false # The key doesn't start with a valid identifier
+      end
+
+      # Test key format : first element need to match the start of the key
+      # We can't have a ssh-rsa start with a ssh-dsa key ...
+      ## ssh-rsa
+      if key_pieces[1].start_with?("AAAAB3NzaC1yc2EA") and key_pieces[0] == "ssh-rsa"
+        return true
+      end
+      ## ssh-dss
+      if key_pieces[1].start_with?("AAAAB3NzaC1kc3MA") and key_pieces[0] == "ssh-dss"
+        return true
+      end
+
+      # if no test return true, the key is invalid
+      return false
+
+    else
+      return false # no ssh key
+    end
+  end
 end
--- routes/keys.rb
+++ routes/keys.rb
@@ -3,6 +3,46 @@ class MyApp < Sinatra::Application
   get "/keys" do
     env['warden'].authenticate!
     @active_tab = "keys"
+    @ssh_keys = env['warden'].user.ssh_keys
     haml "keys/index".to_sym
+  end
+
+  get "/keys/new" do
+    env['warden'].authenticate!
+    @active_tab = "keys"
+    @ssh_key = SshKey.new
+    haml "keys/new".to_sym
+  end
+
+  post "/keys" do
+    env['warden'].authenticate!
+    redirect "/keys/new", :error => "invalid key" unless SshKey.valid?(params[:ssh_key])
+    key = SshKey.create(:name => params[:name], :ssh_key => params[:ssh_key], :user_id => env["warden"].user.id)
+    key.save
+    redirect "/keys", :notice => "ssh key added."
+  end
+
+  get "/keys/:id" do
+    env['warden'].authenticate!
+    @ssh_key = SshKey.get(params[:id])
+    @active_tab = "keys"
+    haml "keys/show".to_sym
+  end
+
+  get "/keys/:id/edit" do
+    env['warden'].authenticate!
+    @ssh_key = SshKey.get(params[:id])
+    @active_tab = "keys"
+    haml "keys/edit".to_sym
+  end
+
+  post "/keys/:id/update" do
+    env['warden'].authenticate!
+    redirect "/keys/#{params[:id]}/edit", :error => "invalid key" unless SshKey.valid?(params[:ssh_key])
+    ssh_key = SshKey.get(params[:id])
+    ssh_key.name = params[:name]
+    ssh_key.ssh_key = params[:ssh_key]
+    ssh_key.save
+    redirect "/keys/", :notice => "key updated."
   end
 end
