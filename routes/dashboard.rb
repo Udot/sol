a55cbb190a0bb2d3682eb6fae6279adbc6c3d06e
mcansky <riboulet@gmail.com>
2011-08-29 01:08:57 +0200

    adding eggs / repository status board in dashboard removing rev from eggs board

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -3,6 +3,7 @@ class MyApp < Sinatra::Application
   get "/dashboard" do
     env['warden'].authenticate!
     @active_tab = "dashboard"
+    @eggs = env['warden'].user.eggs
     @services = Array.new
     @services << {"name" => "Git gate", "description" => "Gateway to the trove", "status" => MercureApi.gate_status, "url" => "#"}
     haml "private/index".to_sym
