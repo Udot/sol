35cca843450c594c8febba4793daa82d6bc31624
mcansky <riboulet@gmail.com>
2011-09-05 15:25:48 +0200

    simplifying landing page, adding contact, about, and fixing top menu

--- routes/dashboard.rb
+++ routes/dashboard.rb
@@ -8,4 +8,14 @@ class MyApp < Sinatra::Application
     @services << {"name" => "Git gate", "description" => "Gateway to the trove", "status" => MercureApi.gate_status, "url" => "#"}
     haml "private/index".to_sym
   end
+
+  get "/about" do
+    @active = "about"
+    haml :"public/about"
+  end
+
+  get "/contact" do
+    @active = "contact"
+    haml :"public/contact"
+  end
 end
