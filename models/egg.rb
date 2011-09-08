3d1b3ff9a48146a49bb51fc7c898a65246652d57
mcansky <riboulet@gmail.com>
2011-09-08 22:14:37 +0200

    methods to generate unicorn and nginx conf

--- models/egg.rb
+++ models/egg.rb
@@ -11,4 +11,53 @@ class Egg
   property :user_id, Integer
   property :created_at, DateTime
   property :updated_at, DateTime
+  # unicorn config
+  property :unicorn_workers, Integer                   # unicorn workers qty (default = 2)
+  property :unicorn_port, Integer                      # unicorn port
+  property :fqdn, String, :unique => true              # user chosen fqdn
+  property :synth_fqdn, String                         # randomly generated fqdn -> need to have an internal DNS
+  property :database_done, Boolean, :default => false
+
+  # return a proper unicorn config
+  def unicorn_conf
+    shared = "/var/www/hosts/#{git_repository.path}/shared"
+    config = ""
+    config += "listen #{unicorn_port}, :tcp_nopush => true\n"
+    config += "worker_processes #{unicorn_workers}\n"
+    config += "user cuddy, www-data\n"
+    config += "pid #{shared}/unicorn_#{git_repository.path}.pid\n"
+    config += "working_directory /var/www/hosts/#{git_repository.path}/current\n"
+    config += "stderr_path #{shared}/log/stderr.log\n"
+    config += "stdout_path #{shared}/log/stdout.log\n"
+    return config
+  end
+
+  # return proper nginx conf
+  def nginx_conf
+    config = ""
+    config += "server {\n"
+    config += "\tlisten\t\t\t80;\n"
+    config += "\tserver_name  #{fqdn};\n"
+    config += "\taccess_log  off;\n"
+    config += "\terror_log off;\n"
+    config += "\t# proxy to unicorn\n"
+    config += "\tlocation / {\n"
+    config += "\t\tproxy_pass         http://#{dragon.public_address}:#{unicorn_port}/;\n"
+    config += "\t\tproxy_redirect     off;\n"
+    config += "\t\tproxy_set_header   Host             $host;\n"
+    config += "\t\tproxy_set_header   X-Real-IP        $remote_addr;\n"
+    config += "\t\tproxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;\n"
+    config += "\t\tproxy_max_temp_file_size 0;\n"
+    config += "\t\tclient_max_body_size       10m;\n"
+    config += "\t\tclient_body_buffer_size    128k;\n"
+    config += "\t\tproxy_connect_timeout      90;\n"
+    config += "\t\tproxy_send_timeout         90;\n"
+    config += "\t\tproxy_read_timeout         90;\n"
+    config += "\t\tproxy_buffer_size          4k;\n"
+    config += "\t\tproxy_buffers              4 32k;\n"
+    config += "\t\tproxy_busy_buffers_size    64k;\n"
+    config += "\t\tproxy_temp_file_write_size 64k;\n"
+    config += "\t}\n"
+    config += "}\n"
+  end
 end
