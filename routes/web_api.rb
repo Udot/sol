1d0be2ecda7f5fd377d694b9e32fc55c3e174491
mcansky <riboulet@gmail.com>
2011-09-08 22:15:31 +0200

    api to register, get unicorn config etc

---
+++ routes/web_api.rb
@@ -0,0 +1,63 @@
+# encoding : utf-8
+class MyApp < Sinatra::Application
+  ## routes allowing the ssh shell to check authorizations and repositories
+  ## 
+  get '/api/web/unicorn' do
+    if not api_auth(env)
+      # the git lib gateway doesn't have a proper api username and/or token
+      status 401
+      body "Unauthorized / Authentication failed"
+      return
+    end
+    if params[:app_name]
+      app = GitRepository.get(:path => params[:app_name]).egg
+      status 200
+      body {"config" => app.unicorn_conf.to_json}
+    else
+      # one or both params are missing
+      status 400
+      body "some params missing"
+      return
+    end
+  end
+
+  post "/api/server/status/?" do
+    if not api_auth(env)
+      # the git lib gateway doesn't have a proper api username and/or token
+      status 401
+      body "Unauthorized / Authentication failed"
+      return
+    end
+    data = JSON.parse(params)
+    server = Dragon.get(:token => data["token"])
+    server.get_status
+    status 200
+    body "ok"
+  end
+
+  post "/api/web/register/?" do
+    if not api_auth(env)
+      # the git lib gateway doesn't have a proper api username and/or token
+      status 401
+      body "Unauthorized / Authentication failed"
+      return
+    end
+    data = JSON.parse(params)
+    host = data["hostname"]
+    name = host.split('.').first
+    app = Dragon.get(:name => name)
+    app.cuddy_token = data["token"]
+    app.save
+    status 200
+    body "Registered"
+  end
+
+  private
+  def api_auth(the_env)
+    token = the_env['HTTP_TOKEN'] || the_env['TOKEN']
+    username = the_env['HTTP_USERNAME'] || the_env['USERNAME']
+    api_user = ApiUser.first(:login => username)
+    return true if api_user.token == token
+    return false
+  end
+end
